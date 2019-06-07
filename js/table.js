d4c1e880402da9c7c85e57451607aa4ec5694ce0
Norbert Tóth <ntoth1990@gmail.com>
2019-06-07 14:26:20 +0200

    table solved

--- js/table.js
+++ js/table.js
@@ -8,8 +8,8 @@ var kaktuszok = [
     ,
     {
         "nev":"óriás sünkaktusz",
-            "magassag": "50-100 cm",
-            "fenyigeny": "fényigényes",
+            "magassag": "150 cm",
+            "fenyigeny": "világos helyre",
             "vizigeny": "szárazságtűrő",
             "hoigeny": "melegigényes",
 
@@ -45,4 +45,28 @@ document.getElementById("magassag1").innerHTML = kaktuszok[0].magassag;
 document.getElementById("fenyigeny1").innerHTML=kaktuszok[0].fenyigeny;
 document.getElementById("vizegeny1").innerHTML=kaktuszok[0].vizigeny;
 document.getElementById("hoigeny1").innerHTML=kaktuszok[0].hoigeny;
+
+document.getElementById("nev2").innerHTML = kaktuszok[1].nev;
+document.getElementById("magassag2").innerHTML = kaktuszok[1].magassag;
+document.getElementById("fenyigeny2").innerHTML=kaktuszok[1].fenyigeny;
+document.getElementById("vizegeny2").innerHTML=kaktuszok[1].vizigeny;
+document.getElementById("hoigeny2").innerHTML=kaktuszok[1].hoigeny;
+
+document.getElementById("nev3").innerHTML = kaktuszok[2].nev;
+document.getElementById("magassag3").innerHTML = kaktuszok[2].magassag;
+document.getElementById("fenyigeny3").innerHTML=kaktuszok[2].fenyigeny;
+document.getElementById("vizegeny3").innerHTML=kaktuszok[2].vizigeny;
+document.getElementById("hoigeny3").innerHTML=kaktuszok[2].hoigeny;
+
+document.getElementById("nev4").innerHTML = kaktuszok[3].nev;
+document.getElementById("magassag4").innerHTML = kaktuszok[3].magassag;
+document.getElementById("fenyigeny4").innerHTML=kaktuszok[3].fenyigeny;
+document.getElementById("vizegeny4").innerHTML=kaktuszok[3].vizigeny;
+document.getElementById("hoigeny4").innerHTML=kaktuszok[3].hoigeny;
+
+document.getElementById("nev5").innerHTML = kaktuszok[4].nev;
+document.getElementById("magassag5").innerHTML = kaktuszok[4].magassag;
+document.getElementById("fenyigeny5").innerHTML=kaktuszok[4].fenyigeny;
+document.getElementById("vizegeny5").innerHTML=kaktuszok[4].vizigeny;
+document.getElementById("hoigeny5").innerHTML=kaktuszok[4].hoigeny;
 console.log(kaktuszok[0].nev);
