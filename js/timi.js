c5375949ca00aaefe3032961435a2f6fa281e751
Tímea Neslanovic <timy1901@gmail.com>
2019-06-07 13:35:40 +0200

    kész 1 gomb-várok a szilvire

---
+++ js/timi.js
@@ -0,0 +1,3 @@
+function aloeButton() {
+    document.getElementById("aloe").innerHTML = "Az orvosi aloét ősidők óta használják gyógyításra. Évezredeken átívelő karrierjét annak köszönheti, hogy számtalan betegségre, bőrelváltozásra és emésztőszervi problémára sikeresen alkalmazható. Napjainkban a világ számos országában termesztik, olyan helyeken, ahol az éghajlat alkalmas a szabadföldben való nevelésére.";
+}
