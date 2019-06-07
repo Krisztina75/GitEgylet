d2d61b8890ecb3efaa1de7943f5db2cb2017d90e
Krisztina Lajos-Kovács <kovacskrisztinamonika@gmail.com>
2019-06-07 14:28:37 +0200

    Játék/második választás

--- js/jatek.js
+++ js/jatek.js
@@ -1,12 +1,25 @@
 function ValaszkeroGomb() {
-    var elsoValasztas = document.querySelector('#h_karacsonyi');
-    var isKaracsonyi = elsoValasztas.checked;
+    var karacsonyi = document.querySelector('#h_karacsonyi');
+    var isKaracsonyi = karacsonyi.checked;
     var popup = "";
 
     if (isKaracsonyi == true) {
-        popup = 'Helyes válasz! :)';
+        popup = 'Helyes válasz!';
     } else {
-        popup = 'Sajnálom, nem talált.';
+        popup = 'Sajnos nem talált.';
+    }
+    alert(popup);
+}
+
+function ValaszkeroGomb_2() {
+    var tengerisun = document.querySelector('#h_tengerisun');
+    var isTengerisun = tengerisun.checked;
+    var popup = "";
+
+    if (isTengerisun == true) {
+        popup = 'Helyes válasz!';
+    } else {
+        popup = 'Sajnos nem talált.';
     }
     alert(popup);
 }
